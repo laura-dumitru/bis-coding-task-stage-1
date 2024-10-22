e49b62c14e7f8a9e940af342b5fdc30e216dbd49
Laura Dumitru <67209237+laura-dumitru@users.noreply.github.com>
2024-10-22 12:52:00 +0100

    Deleted extra function and refactored the date calculation

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-function updateChristmasDate() {
+function calculateDaysUntil (date){
   const today = new Date();
-  const christmasDay = new Date("2024-12-25");
+  return Math.round((date - today) / (1000 * 60 * 60 * 24));
+}
 
-  const daysUntilChristmas = Math.round((christmasDay - today) / (1000 * 60 * 60 * 24));
+function updateChristmasDate() {
+  const christmasDay = new Date("2024-12-25");
+  const nyDay = new Date("2025-01-01");
+
+  const daysUntilChristmas = calculateDaysUntil(christmasDay);
+  const daysUntilNY = calculateDaysUntil(nyDay);
 
   const christmasElement = document.getElementById("my-element");
   christmasElement.innerText = daysUntilChristmas;
-}
-
-function updateNYDate() {
-  const today = new Date();
-  const nyDay = new Date("2025-01-01");
-
-  const daysUntilNY = Math.round((nyDay - today) / (1000 * 60 * 60 * 24));
-
+  
   const nyElement = document.getElementById("my-element2");
   nyElement.innerText = daysUntilNY;
 }
 
 updateChristmasDate();
-updateNYDate();
