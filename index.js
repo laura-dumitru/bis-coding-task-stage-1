a0d676c25f33df0e06bd3a071fe0b78ec3cdb983
Laura Dumitru <67209237+laura-dumitru@users.noreply.github.com>
2024-10-23 10:58:03 +0100

    Add 3rd element on page and fix issue with minus value

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 function calculateDaysUntil (date){
   const today = new Date();
-  return Math.round((date - today) / (1000 * 60 * 60 * 24));
+if (date < today) {
+  date.setFullYear(today.getFullYear() + 1);
 }
-
+return Math.round((date - today) / (1000 * 60 * 60 * 24));
+  }
+ 
 function updateChristmasDate() {
   const christmasDay = new Date("2024-12-25");
   const nyDay = new Date("2025-01-01");
+  const vDay = new Date("2024-02-14");
 
   const daysUntilChristmas = calculateDaysUntil(christmasDay);
   const daysUntilNY = calculateDaysUntil(nyDay);
+  const daysUntilValentines = calculateDaysUntil(vDay);
 
   const christmasElement = document.getElementById("my-element");
   christmasElement.innerText = daysUntilChristmas;
   
   const nyElement = document.getElementById("my-element2");
   nyElement.innerText = daysUntilNY;
+  
+  const valentinesElement = document.getElementById("my-element3");
+  valentinesElement.innerText = daysUntilValentines;
+  
 }
 
 updateChristmasDate();
