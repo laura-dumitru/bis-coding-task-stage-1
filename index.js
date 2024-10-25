a5f48e4a7d631bed8572419a7d81b83f406badc8
Laura Dumitru <67209237+laura-dumitru@users.noreply.github.com>
2024-10-25 13:56:38 +0100

    Add forEach loop on buttons
    ForEach loop added to loop over the buttons and show the number of days on click.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-function calculateDaysUntil (date){
-  const today = new Date();
-if (date < today) {
-  date.setFullYear(today.getFullYear() + 1);
-}
-return Math.round((date - today) / (1000 * 60 * 60 * 24));
+const nyElement = document.getElementById("my-element2")
+const christmasElement = document.getElementById("my-element")
+const valentinesElement = document.getElementById("my-element3")
+
+function calculateDaysUntil(date) {
+  const today = new Date()
+  if (date < today) {
+    date.setFullYear(today.getFullYear() + 1)
   }
- 
-function updateChristmasDate() {
-  const christmasDay = new Date("2024-12-25");
-  const nyDay = new Date("2025-01-01");
-  const vDay = new Date("2024-02-14");
-
-  const daysUntilChristmas = calculateDaysUntil(christmasDay);
-  const daysUntilNY = calculateDaysUntil(nyDay);
-  const daysUntilValentines = calculateDaysUntil(vDay);
-
-  const christmasElement = document.getElementById("my-element");
-  christmasElement.innerText = daysUntilChristmas;
-  
-  const nyElement = document.getElementById("my-element2");
-  nyElement.innerText = daysUntilNY;
-  
-  const valentinesElement = document.getElementById("my-element3");
-  valentinesElement.innerText = daysUntilValentines;
-  
+  return Math.round((date - today) / (1000 * 60 * 60 * 24))
 }
 
-updateChristmasDate();
+function updateChristmasDate() {
+  const christmasDay = new Date("2024-12-25")
+  const nyDay = new Date("2025-01-01")
+  const vDay = new Date("2024-02-14")
+
+  const daysUntilChristmas = calculateDaysUntil(christmasDay)
+  const daysUntilNY = calculateDaysUntil(nyDay)
+  const daysUntilValentines = calculateDaysUntil(vDay)
+
+  christmasElement.innerText = daysUntilChristmas
+
+  nyElement.innerText = daysUntilNY
+
+  valentinesElement.innerText = daysUntilValentines
+}
+
+updateChristmasDate()
+const headings = document.querySelectorAll("h2")
+
+headings.forEach((heading, index) => {
+  heading.addEventListener("click", () => {
+    if (index === 0) {
+      christmasElement.style.opacity = "1"
+    } else if (index === 1) {
+      nyElement.style.opacity = "1"
+    } else {
+      valentinesElement.style.opacity = "1"
+    }
+  })
+})
