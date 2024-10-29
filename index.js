f95afedf1db427a10af64368dfd1bf426a88fe66
Laura Dumitru <67209237+laura-dumitru@users.noreply.github.com>
2024-10-29 11:32:37 +0000

    Added comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const valentinesElement = document.getElementById("my-element3")
 
 function calculateDaysUntil(date) {
   const today = new Date()
+  // if date is in the past add 1 to fix the minus value
   if (date < today) {
     date.setFullYear(today.getFullYear() + 1)
   }
@@ -15,6 +16,7 @@ function updateChristmasDate() {
   const nyDay = new Date("2025-01-01")
   const vDay = new Date("2024-02-14")
 
+  // calling the function calculateDaysUntil to calculate the days from today to each target day and storing the result in a variable
   const daysUntilChristmas = calculateDaysUntil(christmasDay)
   const daysUntilNY = calculateDaysUntil(nyDay)
   const daysUntilValentines = calculateDaysUntil(vDay)
@@ -29,6 +31,7 @@ function updateChristmasDate() {
 updateChristmasDate()
 const headings = document.querySelectorAll("h2")
 
+//Looping over all the headings and adding an event listener. On click each element is shown one at a time not all together
 headings.forEach((heading, index) => {
   heading.addEventListener("click", () => {
     if (index === 0) {
